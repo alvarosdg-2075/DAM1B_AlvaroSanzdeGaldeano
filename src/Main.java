c7b9572297eccdd6edbc0435d1b432be2339cd1b
alvaro <asanzdeech@educacion.navarra.es>
2025-01-07 13:30:19 +0100

    Creación de clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("*******************************************************");
+        System.out.println("Bienvenido al juego del explorador");
+        System.out.println("*******************************************************");
+
+        Mapa mapa = new Mapa();
+        mapa.mostrar();
+
     }
 }
