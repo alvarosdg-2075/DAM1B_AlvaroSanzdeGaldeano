9eeff64bcd036c75e9cccf222c01c9b40a9719f1
alvaro <asanzdeech@educacion.navarra.es>
2025-01-07 13:56:24 +0100

    Creación de clase Explorador (Constructor, getters y setters)

--- src/Explorador.java
+++ src/Explorador.java
@@ -1,4 +1,28 @@
+import java.util.Random;
 public class Explorador {
     private String nombre;
     private Posicion posicionActual;
+    private static final int ARRIBA = 1;
+    private static final int ABAJO = 2;
+    private static final int DERECHA = 3;
+    private static final int IZQUIERDA = 4;
+
+    public Explorador(String nombre) {
+        this.nombre = nombre;
+        Random random = new Random();
+        int fila = random.nextInt(6);
+        this.posicionActual = new Posicion(fila, 0);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Posicion getPosicionActual() {
+        return posicionActual;
+    }
+
+    public void setPosicionActual(Posicion posicionActual) {
+        this.posicionActual = posicionActual;
+    }
 }
